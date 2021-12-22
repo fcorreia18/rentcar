5d6d6a3b9fbc1f5e25838635569129585386210b
Francisco Correia <francisco.correia1804@gmail.com>
2021-12-22 13:49:56 +0100

    chore: add nodemon

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 app.get("/",(req, res)=>{
-    return res.send("Requisição feita com sucesso")
+    return res.json({
+        message: "Requisição feita com sucesso"
+    })
 });
 app.listen(3333);
