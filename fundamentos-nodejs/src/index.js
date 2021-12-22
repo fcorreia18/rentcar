9f6a8a10be4fb04ae075ce33d4ecd0a102153702
Francisco Correia <francisco.correia1804@gmail.com>
2021-12-22 11:14:24 +0100

    docs: first request

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 app.get("/",(req, res)=>{
-    res.send("Requisição feita com sucesso")
+    return res.send("Requisição feita com sucesso")
 });
 app.listen(3333);
