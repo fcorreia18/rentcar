20d48bab2b165402bd1ff568a645e119dff909af
Francisco Correia <francisco.correia1804@gmail.com>
2021-12-24 13:16:23 +0100

    docs: Route Params

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const app = express();
+
+//ROUTE PARAMS: servem para identificar um recurso, no caso de editar, deletar ou buscar
+
 app.get("/courses",(req, res)=>{
     return res.json(["curso 1","curso 2","curso 3","curso 4"])
 });
@@ -13,7 +16,7 @@ app.put("/courses/:id", (req, res)=>{
     return res.json(["curso 6","curso 2","curso 3","curso 4"])
 })
 
-app.patch("/courses/:id",()=>{
+app.patch("/courses/:id",(req, res)=>{
     return res.json(["curso 6","curso 7","curso 3","curso 4"])
 })
 
