25be0720a360b0d1ab7a6653d2ab97d2df9ef989
Francisco Correia <francisco.correia1804@gmail.com>
2021-12-24 14:35:29 +0100

    docs: using HTTP methods done

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json())
 
 //ROUTE PARAMS: servem para identificar um recurso, no caso de editar, deletar ou buscar
-
+//QUERY PARAMS: paginação/filtro
+//BODY PARAMS:Os objectos inserção/alteração (JSON)
 app.get("/courses",(req, res)=>{
+    const query = req.query;
+    console.log(query)
     return res.json(["curso 1","curso 2","curso 3","curso 4"])
 });
 
 app.post("/courses", (req, res)=>{
+    const body = req.body;
+    console.log(body)
     return res.json(["curso 1","curso 2","curso 3","curso 4"])
 })
 
