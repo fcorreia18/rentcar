f928ed2592e13a6c9774fbf517a8496af4c7fa53
Francisco Correia <francisco.correia1804@gmail.com>
2021-12-25 20:20:52 +0100

    feat: create Account functionality add

--- FinAPI/src/index.js
+++ FinAPI/src/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const {v4: uuidv4} = require('uuid');
 const app = express();
+app.use(express.json());
 
 const costumer = [];
 
 app.post("/account",(req, res)=>{
     const {name, bi} = req.body;
+    const id = uuidv4();
+    costumer.push({
+        name, 
+        bi, 
+        id,
+        statement:[]
+    })
     res.status(201).send();
 });
 
