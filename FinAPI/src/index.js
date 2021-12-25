310cfb75c463e4f69bfc37a5354f043e24430181
Francisco Correia <francisco.correia1804@gmail.com>
2021-12-25 20:41:19 +0100

    refactor: create Account functionality changed

--- FinAPI/src/index.js
+++ FinAPI/src/index.js
@@ -8,6 +8,13 @@ const costumer = [];
 app.post("/account",(req, res)=>{
     const {name, bi} = req.body;
     const id = uuidv4();
+
+    const biAlreadyExists = costumer.some((data) => bi === data.bi);
+    if (biAlreadyExists) {
+        return res.status(400).json({
+            error: "BI already exists"
+        })
+    }
     costumer.push({
         name, 
         bi, 
