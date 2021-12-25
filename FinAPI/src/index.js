d6b4d38df2cf2e2ae09ded62ae7f3a522adaf391
Francisco Correia <francisco.correia1804@gmail.com>
2021-12-25 19:33:15 +0100

    chore: add uuid

--- FinAPI/src/index.js
+++ FinAPI/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get("/",(req, res)=>{
-    res.json({message:"alrigth"})
+const costumer = [];
+
+app.post("/account",(req, res)=>{
+    const {name, bi} = req.body;
+    res.status(201).send();
 });
 
 app.listen(3333);
