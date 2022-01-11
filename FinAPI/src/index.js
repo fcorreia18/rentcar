31ee9721cadc5a2ae013132a334ac6eedd207f71
Francisco Correia <francisco.correia1804@gmail.com>
2022-01-11 14:39:55 +0100

    feat: Debit funcionality done, getBalance fixed

--- FinAPI/src/index.js
+++ FinAPI/src/index.js
@@ -19,7 +19,7 @@ const verifyIfExistsAccountBi = (req, res, next) =>{
 }
 const getBalance = (statement)=>{
 const value = statement.reduce((acc, operation)=>{
-    if (operation.type = "deposit") {
+    if (operation.type === "credit") {
         return acc + operation.amount;
     }else{
         return acc - operation.amount;
@@ -59,17 +59,29 @@ app.post("/deposit",verifyIfExistsAccountBi,(req, res)=>{
         description,
         amount,
         created_at: new Date(),
-        type: "deposit"
+        type: "credit"
     }
     customer.statement.push(statementOperation);
     res.status(201).send();
     console.log(getBalance(customer.statement));
 })
 
-app.get("/withdraw", verifyIfExistsAccountBi, (req, res)=>{
-    const customer = req;
+app.post("/withdraw", verifyIfExistsAccountBi, (req, res)=>{
+    const {customer} = req;
     const {amount} = req.body;
+    const balance = getBalance(customer.statement);
 
+    if (balance < amount) {
+        return res.status(400).json({error:"unsuficient funds!!!"})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at:new Date(),
+        type:"debit"
+    }
+    customer.statement.push(statementOperation);
+    res.status(201).send();
 })
 
 app.listen(3333);
