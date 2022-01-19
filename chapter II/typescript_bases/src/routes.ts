37b8e75e4afff88160e86694101291377a1c267a
Francisco Correia <francisco.correia1804@gmail.com>
2022-01-19 21:06:44 +0100

    docs: default values to parameters

--- chapter II/typescript_bases/src/routes.ts
+++ chapter II/typescript_bases/src/routes.ts
@@ -2,7 +2,7 @@ import {Request, Response} from 'express';
 import CreateCourseService from './CreateCourseService';
 
 export function createCourse(req:Request, res:Response){
-    CreateCourseService.execute({educator:"Francisco Correia", name:"Node.js", duration:12});
+    CreateCourseService.execute({educator:"Francisco Correia", name:"Node.js"});
 
     return res.send();
 }
