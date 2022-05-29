952deafd4c04fd74e2f8d081e3737e1592a4ed1f
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-05-29 12:25:08 +0100

    feat: creating categories array

--- chapter II/rent-car/src/routes/categories.routes.ts
+++ chapter II/rent-car/src/routes/categories.routes.ts
@@ -1,5 +1,10 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 
 const categoryRoutes = Router();
-
+const categories = [];
+categoryRoutes.post("/", (req: Request, res: Response): Response => {
+    const { name, description } = req.body;
+    categories.push({ name, description });
+    return res.status(201);
+});
 export { categoryRoutes };
