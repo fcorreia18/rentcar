adc8ef00b948171c1c90753beb221b2553af18e5
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-05-29 17:40:00 +0100

    feat: adding id as uuid to category

--- chapter II/rent-car/src/routes/categories.routes.ts
+++ chapter II/rent-car/src/routes/categories.routes.ts
@@ -1,10 +1,12 @@
 import { Request, Response, Router } from "express";
+import { v4 as uuidV4 } from "uuid";
 
 const categoryRoutes = Router();
 const categories = [];
 categoryRoutes.post("/", (req: Request, res: Response): Response => {
     const { name, description } = req.body;
-    categories.push({ name, description });
-    return res.status(201);
+    const category = { id: uuidV4(), name, description };
+    categories.push(category);
+    return res.status(201).json(categories);
 });
 export { categoryRoutes };
