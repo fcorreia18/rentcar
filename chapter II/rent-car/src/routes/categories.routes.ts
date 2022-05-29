ed7759116f5cf3380c3399a9ce9aabef7b51cd0f
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-05-29 18:20:57 +0100

    fix: add created_at to category

--- chapter II/rent-car/src/routes/categories.routes.ts
+++ chapter II/rent-car/src/routes/categories.routes.ts
@@ -8,7 +8,7 @@ const categories: Category[] = [];
 categoryRoutes.post("/", (req: Request, res: Response): Response => {
     const { name, description } = req.body;
     const category = new Category();
-    Object.assign(category, { name, description });
+    Object.assign(category, { name, description, created_at: new Date() });
     categories.push(category);
     return res.status(201).json(categories);
 });
