6e9a53c9db664a658f36a1947021d50c56db589b
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-05-29 15:22:31 +0100

    docs: new package.json add

--- chapter II/rent-car/src/server.ts
+++ chapter II/rent-car/src/server.ts
@@ -1,6 +1,10 @@
 import express from "express";
 
+import { categoryRoutes } from "./routes/categories.routes";
+
 const app = express();
+app.use(express.json());
+app.use(categoryRoutes);
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello world" });
