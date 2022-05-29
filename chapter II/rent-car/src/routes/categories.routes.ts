2908e01abddacb69684e6d78948f8767655a37e2
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-05-29 18:19:50 +0100

    feat: refactor create category

--- chapter II/rent-car/src/routes/categories.routes.ts
+++ chapter II/rent-car/src/routes/categories.routes.ts
@@ -1,11 +1,14 @@
 import { Request, Response, Router } from "express";
 import { v4 as uuidV4 } from "uuid";
 
+import Category from "../models/Category";
+
 const categoryRoutes = Router();
-const categories = [];
+const categories: Category[] = [];
 categoryRoutes.post("/", (req: Request, res: Response): Response => {
     const { name, description } = req.body;
-    const category = { id: uuidV4(), name, description };
+    const category = new Category();
+    Object.assign(category, { name, description });
     categories.push(category);
     return res.status(201).json(categories);
 });
