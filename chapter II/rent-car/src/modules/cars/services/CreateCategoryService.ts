e0d816d1e9d44905cf351de0bf9695e6d1e830d9
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-06-02 21:47:00 +0100

    fix: fixing the imports

--- chapter II/rent-car/src/modules/cars/services/CreateCategoryService.ts
+++ chapter II/rent-car/src/modules/cars/services/CreateCategoryService.ts
@@ -1,4 +1,4 @@
-import Category from "../../../models/Category";
+import Category from "../models/Category";
 import ICategoriesRepository from "../repositories/ICategoriesRepository";
 
 interface IRequest {
