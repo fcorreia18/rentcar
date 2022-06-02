701147dd20a08e5d309f5c0c5eb66dc77da99508
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-06-02 12:50:20 +0100

    feat: using modules to separate functionalities about specifics parts of the project

--- chapter II/rent-car/src/routes/specification.routes.ts
+++ chapter II/rent-car/src/routes/specification.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
-import SpecificationRepository from "../repositories/SpecificationRepository";
-import CreateSpecificationService from "../services/CreateSpecificationService";
+import SpecificationRepository from "../modules/cars/repositories/SpecificationRepository";
+import CreateSpecificationService from "../modules/cars/services/CreateSpecificationService";
 
 const specificationRoutes = Router();
 const specificationRepository = new SpecificationRepository();
