85ef8acaf71ede023a0a17b9bedcd56a4fca9844
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-06-05 23:48:37 +0100

    fea: read file using he csv-parse package

--- chapter II/rent-car/src/modules/cars/useCases/ImportCategories/ImportCategoriesUseCase.ts
+++ chapter II/rent-car/src/modules/cars/useCases/ImportCategories/ImportCategoriesUseCase.ts
@@ -1,6 +1,15 @@
+// eslint-disable-next-line import/no-unresolved
+import { parse } from "csv-parse";
+// eslint-disable-next-line import/no-unresolved
+import fs from "fs";
+
 export default class ImportCategoriesUseCase {
     execute(file: Express.Multer.File): void {
-        //  const import = this.categoriesRepository.create
-        return console.log(file);
+        const stream = fs.createReadStream(file.path);
+        const parseFile = parse();
+        stream.pipe(parseFile);
+        parseFile.on("data", async (line) => {
+            console.log(line);
+        });
     }
 }
