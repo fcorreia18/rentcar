180f9ee0bfd872694a573b4bc8fc621189355757
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-06-05 08:30:00 +0100

    feat: add multer to upload files

--- chapter II/rent-car/src/routes/categories.routes.ts
+++ chapter II/rent-car/src/routes/categories.routes.ts
@@ -1,9 +1,11 @@
 import { Request, Response, Router } from "express";
+import multer from "multer";
 
 import { categoryController } from "../modules/cars/useCases/CreateCategory/index";
 import { listCategoriesController } from "../modules/cars/useCases/ListCategories";
 
 const categoryRoutes = Router();
+const upload = multer({ dest: "./tmp" });
 categoryRoutes.post("/", (req: Request, res: Response) =>
     categoryController.handle(req, res)
 );
@@ -12,4 +14,10 @@ categoryRoutes.get(
     (req: Request, res: Response): Response =>
         listCategoriesController.handle(req, res)
 );
+categoryRoutes.post("/import", upload.single("file"), (req, res) => {
+    const { file } = req;
+    console.log(file);
+
+    res.status(200).send();
+});
 export { categoryRoutes };
