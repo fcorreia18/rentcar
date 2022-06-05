075b1a46532d65936ade358fa007e43e9d437c2a
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-06-05 09:42:42 +0100

    fix: changed the inversion dependency directly from specificationrepository to ISpecificationRepository

--- chapter II/rent-car/src/modules/cars/useCases/ListSpecifications/ListSpecificationsUseCase.ts
+++ chapter II/rent-car/src/modules/cars/useCases/ListSpecifications/ListSpecificationsUseCase.ts
@@ -1,7 +1,7 @@
-import SpecificationRepository from "../../repositories/implementations/SpecificationRepository";
+import ISpecificationRepository from "../../repositories/ISpecificationRepository";
 
 export default class ListSpecificationsUseCase {
-    constructor(private specificationRepository: SpecificationRepository) {}
+    constructor(private specificationRepository: ISpecificationRepository) {}
     execute() {
         const all = this.specificationRepository.list();
         return all;
