1d99d153b0c0065f64f398f029becabfbfa8efd8
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-06-20 22:17:40 +0100

    feat: load categories functionality

--- chapter II/rent-car/src/modules/cars/useCases/ImportCategories/ImportCategoriesUseCase.ts
+++ chapter II/rent-car/src/modules/cars/useCases/ImportCategories/ImportCategoriesUseCase.ts
@@ -3,13 +3,30 @@ import { parse } from "csv-parse";
 // eslint-disable-next-line import/no-unresolved
 import fs from "fs";
 
+import ICategoriesRepository from "../../repositories/ICategoriesRepository";
+
+interface IImportCategory {
+    name: string;
+    description: string;
+}
 export default class ImportCategoriesUseCase {
-    execute(file: Express.Multer.File): void {
+    constructor(private categoriesRepository: ICategoriesRepository) {}
+
+    loadCategories(file: Express.Multer.File) {
+        const categories: IImportCategory[] = [];
         const stream = fs.createReadStream(file.path);
         const parseFile = parse();
         stream.pipe(parseFile);
         parseFile.on("data", async (line) => {
-            console.log(line);
+            const [name, description] = line;
+            categories.push({
+                name,
+                description,
+            });
         });
+        return categories;
+    }
+    execute(file: Express.Multer.File): void {
+        console.log(this.loadCategories(file));
     }
 }
