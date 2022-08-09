5d2bc922618f16dcf837cba9a4a4993eae75d6df
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-08-10 00:09:18 +0100

    feat: createCategories migration done, typescript and ts-node-dev updated to last version

---
+++ chapter II/rent-car/src/database/migrations/1660084956240-CreateCategories.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+export class CreateCategories1660084956240 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "categories",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "uuid",
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                    },
+                    {
+                        name: "description",
+                        type: "varchar",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                ],
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("categories");
+    }
+}
