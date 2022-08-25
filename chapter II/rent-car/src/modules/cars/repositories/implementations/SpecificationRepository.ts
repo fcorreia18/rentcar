5249fbe277bdbd522b382e6d73f3b434e9adc36b
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-08-25 18:29:09 +0100

    feat: specification create functionality done

--- chapter II/rent-car/src/modules/cars/repositories/implementations/SpecificationRepository.ts
+++ chapter II/rent-car/src/modules/cars/repositories/implementations/SpecificationRepository.ts
@@ -2,30 +2,31 @@ import { getRepository, Repository } from "typeorm";
 
 import { ICreateSpecificationDTO } from "../../dtos/CreateSpecificationDTO";
 import { Specification } from "../../entities/Specification";
+import { ISpecificationRepository } from "../ISpecificationRepository";
 
-export class SpecificationRepository {
-    specification: Repository<Specification>;
+export class SpecificationRepository implements ISpecificationRepository {
+    ormRepository: Repository<Specification>;
 
     constructor() {
-        this.specification = getRepository(Specification);
+        this.ormRepository = getRepository(Specification);
     }
 
     public async create({
         name,
         description,
     }: ICreateSpecificationDTO): Promise<void> {
-        const specification = this.specification.create({ name, description });
+        const specification = this.ormRepository.create({ name, description });
 
-        await this.specification.save(specification);
+        await this.ormRepository.save(specification);
     }
 
     public async list(): Promise<Specification[]> {
-        const specifications = await this.specification.find();
+        const specifications = await this.ormRepository.find();
         return specifications;
     }
 
     public async findByName(name: string): Promise<Specification> {
-        const specification = await this.specification.findOne({ name });
+        const specification = await this.ormRepository.findOne({ name });
         return specification;
     }
 }
--- chapter II/rent-car/src/routes/specification.routes.ts
+++ chapter II/rent-car/src/routes/specification.routes.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 
-import { SpecificationRepository } from "../modules/cars/repositories/implementations/SpecificationRepository";
 import { CreateSpecificationController } from "../modules/cars/UseCases/CreateSpecification/CreateSpecificationController";
 
 export const specificationRoutes = Router();
-const specificationRepository = new SpecificationRepository();
 const createSpecificationController = new CreateSpecificationController();
-specificationRoutes.get("/", (req, res) => {
-    res.json({ specifications: specificationRepository.list() });
-});
+// specificationRoutes.get("/", (req, res) => {
+//     res.json({ specifications: specificationRepository.list() });
+// });
 specificationRoutes.post("/", createSpecificationController.handle);
