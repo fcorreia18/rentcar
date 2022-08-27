c64a810c51ac8ae7b559a054874320daec53ddf5
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-08-27 16:27:48 +0100

    feat: IUserRepository and user DTO created

---
+++ chapter II/rent-car/src/modules/accounts/users/repositories/IUsersRepository.ts
@@ -0,0 +1,15 @@
+import { User } from "../entities/User";
+
+interface ICreateUserDTO {
+    name: string;
+    username: string;
+    password: string;
+    email: string;
+    driver_license: string;
+}
+
+export interface IUsersRepository {
+    create(user: ICreateUserDTO): Promise<void>;
+    list(): Promise<User[]>;
+    findByName(name: string): Promise<User | undefined>;
+}
