3a8a61db624ce72ca0f6f73ebb1f5f6688984330
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-08-27 16:21:14 +0100

    feat: user model created

---
+++ chapter II/rent-car/src/modules/accounts/users/entities/User.ts
@@ -0,0 +1,44 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+@Entity("users")
+export class User {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column({ unique: true })
+    username: string;
+
+    @Column()
+    password: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    driver_license: string;
+
+    @Column()
+    isAdmin: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidV4();
+        }
+    }
+}
