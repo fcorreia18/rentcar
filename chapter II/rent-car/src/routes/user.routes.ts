9cbbe85b3fe7edce7ed848e1872586abc7c7d4ce
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-08-27 16:58:21 +0100

    feat: userRoutes created

--- chapter II/rent-car/src/routes/user.routes.ts
+++ chapter II/rent-car/src/routes/user.routes.ts
@@ -1,3 +1,9 @@
 import { Router } from "express";
 
+import { CreateUserController } from "../modules/accounts/users/UseCases/CreateUser/CreateUserController";
+
 const userRoutes = Router();
+const createUserController = new CreateUserController();
+userRoutes.post("/", createUserController.handle);
+
+export { userRoutes };
