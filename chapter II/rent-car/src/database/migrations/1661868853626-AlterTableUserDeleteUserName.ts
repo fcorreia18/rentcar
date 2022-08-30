bc09477d484882f2b7644aaaaaa425020d3f536d
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-08-30 15:32:57 +0100

    feat: typeorm new migration to delete username column from users table

---
+++ chapter II/rent-car/src/database/migrations/1661868853626-AlterTableUserDeleteUserName.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class AlterTableUserDeleteUserName1661868853626
+    implements MigrationInterface
+{
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn("users", "username");
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn(
+            "users",
+            new TableColumn({
+                name: "username",
+                type: "varchar",
+                isUnique: true,
+                isNullable: true,
+            })
+        );
+    }
+}
