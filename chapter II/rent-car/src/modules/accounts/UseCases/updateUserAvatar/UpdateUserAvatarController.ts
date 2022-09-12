f9c0af651e35ab767fc448df771a0e6b98591140
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-09-12 07:55:52 +0100

    fix: user.routes

--- chapter II/rent-car/src/modules/accounts/UseCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ chapter II/rent-car/src/modules/accounts/UseCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import fs from "fs";
 import { container } from "tsyringe";
 
 import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
@@ -6,11 +7,16 @@ import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
 export class UpdateUserAvatarController {
     async handle(req: Request, res: Response): Promise<Response> {
         const { file } = req;
-        const { email } = req.body;
+        fs.promises.unlink(file.path);
+
+        const { path: avatar_file } = file;
+        console.log("path", avatar_file);
+        const { id: user_id } = req.user;
         const updateUserAvatarUseCase = container.resolve(
             UpdateUserAvatarUseCase
         );
-        await updateUserAvatarUseCase.execute(email, file);
+        console.log("id", user_id);
+        await updateUserAvatarUseCase.execute({ user_id, avatar_file });
         return res.send();
     }
 }
--- chapter II/rent-car/src/routes/user.routes.ts
+++ chapter II/rent-car/src/routes/user.routes.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const userRoutes = Router();
+const upload = multer({
+    dest: "./tmp",
+});
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 userRoutes.post("/", createUserController.handle);
+userRoutes.patch(
+    "/avatar",
+    upload.single("avatar"),
+    updateUserAvatarController.handle
+);
 
 export { userRoutes };
