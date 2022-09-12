b65a624d32b10c164a72e115dd829556ed1e1024
FranciscoCorreia28 <francisco.correia1804@gmail.com>
2022-09-12 19:40:09 +0100

    refactor:

--- chapter II/rent-car/src/modules/accounts/UseCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ chapter II/rent-car/src/modules/accounts/UseCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -7,9 +7,9 @@ import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
 export class UpdateUserAvatarController {
     async handle(req: Request, res: Response): Promise<Response> {
         const { file } = req;
-
         const { filename: avatar_file } = file;
         const { id: user_id } = req.user;
+
         const updateUserAvatarUseCase = container.resolve(
             UpdateUserAvatarUseCase
         );
